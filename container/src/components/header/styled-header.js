69cb8556aee0df12d0f7cc97f176dac70ef8de6e
Xavi <xaviercanchal@gmail.com>
2020-02-06 00:31:19 +0100

    feat: initial dev setup

---
+++ container/src/components/header/styled-header.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import Header from './header';
+
+export default styled(Header)`
+  display: flex;
+  height: 60px;
+  box-shadow: 0 1px 0 0 rgba(0,0,0,.1);
+`;
