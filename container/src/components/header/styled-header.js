17736b041a43e782ea78ee81b8f8f5ae65d78eb3
Xavi <xaviercanchal@gmail.com>
2020-02-06 00:34:57 +0100

    Revert "feat: initial dev setup"
    This reverts commit 69cb8556aee0df12d0f7cc97f176dac70ef8de6e.

--- container/src/components/header/styled-header.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-import Header from './header';
-
-export default styled(Header)`
-  display: flex;
-  height: 60px;
-  box-shadow: 0 1px 0 0 rgba(0,0,0,.1);
-`;
